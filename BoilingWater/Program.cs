f46201f845847a32387a0296486e513d0108c991
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 11:16:52 +0300

    Initial commit

---
+++ BoilingWater/Program.cs
@@ -0,0 +1,18 @@
+﻿class BoilingWater
+{
+    public static void Main(String[] args)
+
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 100)
+        {
+            Console.WriteLine("The water is boiling");
+        }
+        else {
+            Console.WriteLine("The water is not hot enough");
+            
+                
+            }
+        }
+    }
